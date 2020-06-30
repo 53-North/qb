eb5bfc5912642e841be092791ca376c2031103f5
Holly <hollyduckworth@hotmail.co.uk>
2020-06-30 15:47:52 +0100

    simplified version of the USERSAFE page ready for a PUT req to be added to it

--- src/pages/UserSafe.js
+++ src/pages/UserSafe.js
@@ -1,22 +1,19 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 function UserSafe() {
+
     return (
 
         <div className="UserSafe">
             <header><h1>Quake°Beacon</h1></header>
-            <p>An earthquake has happened and the epicentre is XXXPROPS NEEDEDXXX from your location.</p>
-            <h3>Enter the number below to confirm your safety</h3>
-            <h1>52</h1> 
-            {/* this should be props and auto generated */}
-            <div className="input-group mb-3">
-                <div class="input-group-prepend">
-                    <button className="btn btn-outline-secondary" type="button">I am Safe</button>
-                </div>
-                <input type="text" className="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1"/>
-            </div>
-            </div>
+            <p>An earthquake has happened and the epicentre is XXXPROPS NEEDEDXXX from your location, <strong>are you safe?</strong></p>
+            <a href ="/UserSafe/" role="button" type="submit" class="btn btn-danger btn-xlg">I AM SAFE </a> 
+
+    
             
+        </div>
+
     );
 }
 
