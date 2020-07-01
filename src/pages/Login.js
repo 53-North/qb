c3ad9310722e52728ed31d22ee9238c7a648557a
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-07-01 11:03:49 +0100

    changed to className, changed h8 to h6

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,13 +7,13 @@ function Login() {
             <h1>Quake°Beacon</h1>
             <form>
                 <div className=" form-group">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                 </div>
                 <a href="/UserSafe/" role="button" type="submit" className="btn btn-dark login_button">log in </a>
             </form>
             <footer className="Login-footer">
-                <h8>don't have an account- <a href="/UserReg/">sign up </a> </h8>
+                <h6>don't have an account- <a href="/UserReg/">sign up </a> </h6>
             </footer>
         </div>
     );
