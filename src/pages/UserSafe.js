16181ba48c7c102fca7ee2560a21b2dc0d69f9a3
FlysJoint <wattoo42@hotmail.com>
2020-07-01 12:13:55 +0100

    can mark user 1 in the db as safe

--- src/pages/UserSafe.js
+++ src/pages/UserSafe.js
@@ -1,40 +1,67 @@
-import React from 'react';
-//import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { render } from 'react-dom';
 
+
+function UserSafe() {
+
+    let thisUser;
+
+    //const [thisUser, setUser] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://15omqaggcl.execute-api.eu-west-2.amazonaws.com/dev/user/")
+            .then(res => {
+            //console.log(res.data);
+            //setUser(res.data.user[0]);
+            thisUser = res.data.user[0];
+            console.log(thisUser);
+            //console.log(setUser);
+            })
+            .catch(err => {
+            console.log("Could not fetch users", err);
+            });
+    }, []);
+
+    function markSafe(id) {
+
+        id = 1; // this line needs to be removed when we get the app using different users
+
+        console.log('clicked mark safe button');
+        // const updatedUsers = users.map( user => {
+        //     if ( user.userId === id) {
+        //         user.user_markedSafe = 1;
+        //     }
+        //     return user;
+        // });
+
+        //const updatedUser = 1;
+        // const updatedUser = users.filter(user => user.userId === id);
+        // console.log(updatedUser);
+
+        thisUser.user_markedSafe = 1;
+        console.log(thisUser);
+
+        axios
+        .put(
+        `https://15omqaggcl.execute-api.eu-west-2.amazonaws.com/dev/user/${id}`, thisUser
+        )
+        .then(res => {
+        // There is probably no data returned from a Put request.
+        // But if you're in the "then" function you know the request succeeded.
+        console.log(id + ' marked safe');
+
+        })
+        .catch(err => {
+        console.log("Error marking " + id + " safe", err);
+        });
+
+        //setUsers( updatedUsers );
+        //setUser( thisUser );
+    }
+
 /*
-function markSafe(id) {
-
-    id = 1; // this line needs to be removed when we get the app using different users
-
-  //console.log(users);
-    const updatedUsers = users.map(user => {
-      if(user.userId === id) {
-        user.isSafe = 1; //
-      }
-      return user;
-    });
-
-    const updatedUser = users.filter(user => user.userId === id);
-    console.log(updatedUser);
-
-    axios
-    .put(
-      `https://15omqaggcl.execute-api.eu-west-2.amazonaws.com/dev/user/${id}`, updatedUser[0]
-    )
-    .then(res => {
-      // There is probably no data returned from a Put request.
-      // But if you're in the "then" function you know the request succeeded.
-      console.log(id + ' marked safe');
-
-    })
-    .catch(err => {
-      console.log("Error marking " + id + " safe", err);
-    });
-
-    setUsers( updatedUsers );
-}
-
 function humanTest() {
     // When a user marks themselves safe they must complete a simple test
     // to ensure the button hasn't been accidentally activated by rubble or debris
@@ -64,17 +91,12 @@ function humanTest() {
 }
 */
 
-function UserSafe() {
-
     return (
 
         <div className="UserSafe">
             <header><h1>Quake°Beacon</h1></header>
             <p>An earthquake has happened and the epicentre is XXXPROPS in here XXX from your location, <strong>are you safe?</strong></p>
-            <a href ="/UserSafe/" role="button" type="submit" class="btn btn-danger btn-xlg">I AM SAFE </a> 
-
-    
-            
+            <a href ="/UserSafe/" role="button" type="submit" className="btn btn-danger btn-xlg" onClick = {() => markSafe()}>I AM SAFE </a> 
         </div>
 
     );
