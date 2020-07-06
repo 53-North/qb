b2b63dbc00e6140d017c8298518bea98b858bc3a
helenp360 <helloupart@gmail.com>
2020-07-06 12:38:38 +0100

    Added functionality for log in using hard coded email addresses

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Login.css';
 
-function Login(props) {
+//cut out logInUser={logInUser} from router section of app.js
 
-    function logInClick() {
+function Login(props) {
+    const [text, setText] = useState("");
+
+    function handleTextChange(event) {
+        console.log(event.target.value);
+        setText(event.target.value);
+      }
+
+    const myUsers = [
+        { myEmail: "helloupart@gmail.com", myUserId: 1 },
+        { myEmail: "joe@gmail.com", myUserId: 2 }
+    ]
+    // const [users, setUsers] = useState([]);
+
+    function logInUser(text) {
         //check the email address in the box is one in the database
         //get all the email addresses in an array
         //then loop through the array and check for a match
         //if so...
-        props.logInUser(props.text);
+        // let idToUse =0;
+        for (let i = 0; i < myUsers.length; i++) {
+            if (myUsers[i].myEmail === text) {
+                // console.log(users[i].phone);
+                console.log(text + " valid email address");
+                window.open("/UserSafe/");
+            }
+        }
         
-        window.open("/UserSafe/");
     }
+
+    function logInClick(event) {
+        //check the email address in the box is one in the database
+        //get all the email addresses in an array
+        //then loop through the array and check for a match
+        //if so...
+
+        // axios
+        // .get("https://15omqaggcl.execute-api.eu-west-2.amazonaws.com/dev/user/")
+        // .then(res => {
+        //     setUsers(res.data.user);
+        // })
+        // .catch(err => {
+        //     console.log("Could not fetch users", err);
+        // });
+
+        console.log(text)
+        logInUser(text);
+
+    }
+
+
 
     return (
         <div className="login-background">
@@ -20,12 +63,23 @@ function Login(props) {
                     Log in below:
                 </h3>
             </div>
-            
+
             <form>
-               
+
                 <div className=" form-group">
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email: name@example.com" onChange={ props.handleTextChange } value={ props.text } />
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="email"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                        placeholder="Email: name@example.com"
+                        onChange={handleTextChange}
+                        value={text}
+                    />
+                    <input type="password"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                        placeholder="Password"
+                    />
 
                     <div className="form-check">
                         <input type="checkbox" className="form-check-input" id="dropdownCheck" />
